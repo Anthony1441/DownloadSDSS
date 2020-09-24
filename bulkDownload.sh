3029e0b374579a2d05bb3b9978b9ac106a80ea18
Anthony1441 <anthonynavarrette2017@gmail.com>
2020-09-23 17:06:10 -0700

    Fixed issues

--- bulkDownload.sh
+++ bulkDownload.sh
@@ -6,4 +6,4 @@ if [[ $# -ne 3 ]]; then echo "USAGE: bulkDownload.sh outDir nameRaDecFile number
 mkdir $1
 
 # run getGal.py on each galaxy in the file
-awk -v num="$3" -v out="$1" '{if (NR > 402) {exit} if (NR > 401) {system("python2 -W ignore getGal.py " $2 " " $3 " " $1 " -out_dir " out " -min_num_stars 15")}}' $2
+awk -v num="$3" -v out="$1" '{if (NR > num) {exit} if (NR > 0) {system("python2 -W ignore getGal.py " $2 " " $3 " " $1 " -out_dir " out " -min_num_stars 15")}}' $2
