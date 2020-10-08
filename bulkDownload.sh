e13e969cce68e5038b75396db724d7decec157c3
Anthony1441 <anthonynavarrette2017@gmail.com>
2020-10-07 17:35:19 -0700

    Fixed finding center of off-axis galaxies

--- bulkDownload.sh
+++ bulkDownload.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-if [[ $# -ne 3 ]]; then echo "USAGE: bulkDownload.sh outDir nameRaDecFile numberToDownload"; exit; fi
+if [[ $# -ne 4 ]]; then echo "USAGE: bulkDownload.sh outDir nameRaDecFile numberToDownload numberToSkip"; exit; fi
 
 mkdir $1
 
 # run getGal.py on each galaxy in the file
-awk -v num="$3" -v out="$1" '{if (NR > num) {exit} if (NR > 0) {system("python2 -W ignore getGal.py " $2 " " $3 " " $1 " -out_dir " out " -min_num_stars 15")}}' $2
+awk -v num="$3" -v out="$1" -v skip="$4" '{if (NR > num) {exit}; if (NR <= skip) {next}; if (NR > 0) {system("python2 -W ignore getGal.py " $2 " " $3 " " $1 " -out_dir " out " -min_num_stars 15")}}' $2
